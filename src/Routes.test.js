298cde386d9bd305d27229488d92d3a0d0adc03f
theUyonator <duyo@ualberta.ca>
2021-08-24 21:52:04 -0600

    react route testing complete

--- src/Routes.test.js
+++ src/Routes.test.js
@@ -2,6 +2,10 @@ import React from "react";
 import Routes from "./Routes";
 import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+import whiskey from "./whiskey.jpg";
+import duke from "./duke.jpg";
+import tubby from "./tubby.jpg";
+import perry from "./perry.jpg";
 
 const dogs = [
     {
@@ -59,4 +63,40 @@ it("renders all dog names existing in intintal doglist at /dogs", () =>{
         expect(linkElement).toBeInTheDocument();
 
     }
+});
+
+it("renders only whiskey's information at /dogs/whiskey", () =>{
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/dogs/whiskey"]}>
+      <Routes dogs={dogs} />
+    </MemoryRouter>
+
+  )
+  const whiskeyInfo = dogs.find(d => d.name === "Whiskey");
+  const dukeInfo = dogs.find(d => d.name === "Duke");
+
+  const listItem = getByText(new RegExp(whiskeyInfo.facts[0], "i"));
+  expect(listItem).toBeInTheDocument();
+
+  expect(screen.queryByText(new RegExp(dukeInfo.facts[0], "i")));
+
+
+})
+
+it("redirects to the homepage when a badroute is hit", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/dogs/jsklddmomd"]}>
+      <Routes dogs={dogs} />
+    </MemoryRouter>
+
+  );
+
+  const dogNames = dogs.map(d => d.name.toLocaleLowerCase());
+    for (const name of dogNames){
+        const linkElement = getByText(new RegExp(name, "i"));
+        expect(linkElement).toBeInTheDocument();
+
+    }
+
+
 })
